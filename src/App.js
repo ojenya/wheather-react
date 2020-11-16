dd8e88ee03075e500647c83acf274d53bcefa4b0
Oseledets.Evgeniy <Oseledets.Evgeniy@zolotoy.ru>
2020-11-16 22:00:45 +0300

    delete GRID,COL,ROW

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React, { Component } from "react";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootswatch/dist/journal/bootstrap.css";
-// import "bootswatch/dist/cerulean/bootstrap.css";
-
-
-import { Navbar, NavItem, Nav, Grid, Row, Col } from "react-bootstrap";
 
 const PLACES = [
   { name: "Palo Alto", zip: "94303" },
@@ -60,36 +54,18 @@ class App extends Component {
   render() {
     const activePlace = this.state.activePlace;
     return (
-      <div>
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              React Simple Weather App
-            </Navbar.Brand>
-          </Navbar.Header>
-        </Navbar>
-        <Grid>
-          <Row>
-            <Col md={4} sm={4}>
-              <h3>Select a city</h3>
-              <Nav
-                bsStyle="pills"
-                stacked
-                activeKey={activePlace}
-                onSelect={index => {
-                  this.setState({ activePlace: index });
-                }}
-              >
-                {PLACES.map((place, index) => (
-                  <NavItem key={index} eventKey={index}>{place.name}</NavItem>
-                ))}
-              </Nav>
-            </Col>
-            <Col md={8} sm={8}>
-              <WeatherDisplay key={activePlace} zip={PLACES[activePlace].zip} />
-            </Col>
-          </Row>
-        </Grid>
+      <div className="App">
+        {PLACES.map((place, index) => (
+          <button
+            key={index}
+            onClick={() => {
+              this.setState({ activePlace: index });
+            }}
+          >
+            {place.name}
+          </button>
+        ))}
+        <WeatherDisplay key={activePlace} zip={PLACES[activePlace].zip} />
       </div>
     );
   }
